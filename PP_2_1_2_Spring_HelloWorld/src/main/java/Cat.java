b260509d44873229b1472fd24e0b49e9c87a5bee
Artyom Burnaev <149198360+MacheteNurbaev@users.noreply.github.com>
2024-01-11 17:53:32 +0300

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String string) {
+        this.message = message;
+    }
+
+}
